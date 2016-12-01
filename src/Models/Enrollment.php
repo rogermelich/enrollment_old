5a8c17bbbfbc3411cb2e2ba277fabe599e4dd645
Roger Melich <rogermelich@gmail.com>
2016-12-01 20:42:52 +0100

    Install l5-repository and implements Transformable into Enrollment Model

--- src/Models/Enrollment.php
+++ src/Models/Enrollment.php
@@ -3,9 +3,13 @@
 namespace Scool\Enrollment\Models\Enrollment;
 
 use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
 
-class Enrollment extends Model
+
+class Enrollment extends Model implements Transformable
 {
+	use TransformableTrait;
     protected $table = 'enrollments';
     protected $fillable = ['validated', 'finished', 'study_id', 'course_id', 'classroom_id'];
     protected $guarded = ['id'];
