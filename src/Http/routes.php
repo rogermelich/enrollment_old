3b092db1a16f798da16beb39ca78c6846efa3545
Roger Melich <rogermelich@gmail.com>
2016-12-13 16:20:06 +0100

    Add middleware web and api

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,18 +1,18 @@
 <?php
 
-Route::group(['prefix' => 'api/v1.0', 'middleware' => ['auth.api']], function () {
-    Route::resource('enrollment', 'EnrollmentsController');
+Route::group([
+    'middleware' => 'web'], function(){
+    Route::group(['middleware' => ['auth']], function () {
+        Route::resource('enrollment', 'EnrollmentsController');
+    });
 });
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::resource('enrollment', 'EnrollmentsController');
 
-    Route::get('/test', function () {
-        DB::listen(function ($event) {
-            dump($event->sql);
-            dump($event->bindings);
-        });
-        Stats::of(Scool\Enrollment\Models\Enrollment\Enrollment::class);
-        return Stats::total();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'api',
+], function() {
+    Route::group(['prefix' => 'api/v1.0', 'middleware' => ['auth.api']], function () {
+        Route::resource('enrollment', 'EnrollmentsController');
     });
 });
