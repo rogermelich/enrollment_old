f22f96569b1a5c8d1f8c17af67d7d2b0ca378af8
Roger Melich <rogermelich@gmail.com>
2017-01-19 21:14:28 +0100

    Add EnrollmentPermissionSeeder

---
+++ src/Database/Seeds/EnrollmentPermissionsSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Contracts\Permission;
+use Spatie\Permission\Contracts\Role;
+
+class EnrollmentPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::create(['name' => 'browse enrollments']);
+        Permission::create(['name' => 'read enrollments']);
+        Permission::create(['name' => 'edit enrollments']);
+        Permission::create(['name' => 'add enrollments']);
+        Permission::create(['name' => 'delete enrollments']);
+        $role = Role::create(['name' => 'manage enrollments']);
+        $role->givePermissionTo('browse enrollments');
+        $role->givePermissionTo('read enrollments');
+        $role->givePermissionTo('edit enrollments');
+        $role->givePermissionTo('add enrollments');
+        $role->givePermissionTo('delete enrollments');
+    }
+}
